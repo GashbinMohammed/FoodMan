59c8dcae7bce4369b0567c9e104f82dd140048ea
GashbinMohammed <gashbinmhamed21@gmail.com>
2024-10-08 17:00:10 +0300

    Add files via upload

---
+++ js/home.js
@@ -0,0 +1,21 @@
+ScrollReveal().reveal('.header_image,.chefs__image', {
+    distance: '50px',
+    origin: 'right',
+    duration: 1000,
+  
+  });
+  ScrollReveal().reveal('.explore__image', {
+    distance: '50px',
+    origin: 'left',
+    duration: 1000,
+  
+  });
+  ScrollReveal().reveal('.main__header,.section__description,.header__btn,.Chef__header,.chef__description,.chef__list,.explore__description,.explore__btn', {
+    distance: '50px',
+    origin: 'bottom',
+    duration: 1000,
+    interval: 300,
+
+  });
+
+  
